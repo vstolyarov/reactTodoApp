fd98ea98a4f72b7b6b37c83a7a3818295dcce4ab
vstolyarov <vstolyarov@yahoo.com>
2019-09-17 19:31:46 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+constructor() {
+  super();
+  this.state = {
+     message: 'Hello',
+     newTodo: '',
+     todos: [{
+      title: 'Learn React',
+      done: false
+     },
+     {
+      title: 'Learn JSX',
+      done: false
+    }]
+  };
 }
+
+newTodoChanged=(e)=> {
+  console.log(e.target.value);
+  this.setState({newTodo:e.target.value})
+}
+formSubmitted=(e)=>{
+  e.preventDefault();
+  console.log(this.state.newTodo)
+  this.setState({
+    todos:[...this.state.todos, {
+      title: this.state.newTodo,
+      done: false
+  }]
+});
+
+console.log(this.state.todos)
+}
+
+render() {
+    return (
+      <div className="App">
+        <h3>{this.state.message}</h3>
+      <form onSubmit={this.formSubmitted}>
+        <label htmlFor="newTodo">New Todo</label>
+        <input onChange={this.newTodoChanged} id="newTodo" name="newTodo" value={this.state.newTodo}/>      
+        <button type="submit">Add Todo</button>
+        </form>
+        <ul>
+          {this.state.todos.map((todo,i)=>{
+            return <li key={i}>{todo.title}</li>
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+  
 
 export default App;
